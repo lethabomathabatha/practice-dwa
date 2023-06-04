3728bb440c497c4cebf0dc13936e2b19aee88ce8
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-04 11:24:14 +0200

    added example code

---
+++ data-count-app/components/single-task.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const template = document.createElement("template");
+
+template.innerHTML = /* html */ `
+    <li class= "task">
+    <label class="task__check">
+        <input class="task__input" type="checkbox">
+    </label>
+    
+    <button class="task__title"></button>
+    
+    `;
+
+    // 
