21056376a5d2ac501ef6a7f0b07317312e0bef5f
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-13 08:42:48 +0200

    added HOF example

---
+++ higher-order-functions/scripts.js
@@ -0,0 +1,83 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
+
+console.log(html, render)
+
+
+const tasks = [
+    { 
+        id: 1, 
+        title: 'Task 1', 
+        completed: true,
+    },
+    { 
+        id: 2, 
+        title: 'Task 2', 
+        completed: false 
+    },
+    { 
+        id: 3, 
+        title: 'Task 3', 
+        completed: true 
+    },
+    { 
+        id: 4, 
+        title: 'Task 4', 
+        completed: false 
+    },
+  ];
+
+  // add note to each task
+  for (let i = 0; i < tasks.length; i++) {
+    tasks[i].note = `${tasks[i].title} is this way because of your actions.`;
+  }
+
+const completedTasks = tasks.filter((task) =>  task.completed);
+const incompleteTasks = tasks.filter((task) => !task.completed);
+const taskNotes = tasks.map((task) => task.note);
+
+
+console.log('Completed tasks: ');
+console.log(completedTasks);
+
+console.log('Incomplete tasks: ');
+console.log(incompleteTasks);
+
+console.log('Task notes: ');
+console.log(taskNotes);
+
+
+
+/**
+ * Returns an HTML list item element containing the title of a task and the corresponding completion status.
+ *
+ * @param {array} tasks - An array of tasks objects containing a title property.
+ * @param {boolean} completed - Whether the task is completed or not.
+ * @return {string} The HTML list item element as a string.
+ */
+const createTaskList = (tasks) => {
+    return html `
+        <li>${tasks.title + ' - ' + tasks.completed + ' '}<button>${tasks.id}</button></li>
+    
+        <style>
+            li {
+                color: blue;
+                font-size: 1.2rem;
+            }
+
+            button {
+                color: blue;
+            }
+        </style>
+    `
+    // adding styling
+}
+createTaskList(tasks);
+
+// when the buttons are clicked, the task note should appear as an overlay
+
+
+const taskListElement = document.getElementById('taskList');
+const taskMarkup = tasks.map((task) => createTaskList(task));
+
+render(taskMarkup, taskListElement);
